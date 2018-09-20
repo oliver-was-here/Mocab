1a5d5aa276afc5e892288319a4eeec3722efdb19
Oliver Scott <oliverhbscott@gmail.com>
2018-09-19 21:04:52 -0400

    code cleanup

--- Mocab/AppDelegate.swift
+++ Mocab/AppDelegate.swift
@@ -3,7 +3,6 @@ import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
     static private let CURRENT_WORD_KEY = "wordInProgress"
     private var center: UNUserNotificationCenter {
@@ -20,22 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        print("background")
-        
         configureWordNotifications()
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        print("terminated")
     }
 }
 
@@ -72,4 +56,3 @@ extension AppDelegate {
         }
     }
 }
-
