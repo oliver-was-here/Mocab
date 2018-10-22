af6983d046f245fac22f9316a8ae6477f53f34f9
Oliver Scott <oliverhbscott@gmail.com>
2018-10-21 21:01:09 -0400

    bug fix - provide more lenient deserialization strategy for Oxford Service due to variable return schema

--- Mocab/Services/Responses/DefinitionResponse.swift
+++ Mocab/Services/Responses/DefinitionResponse.swift
@@ -14,8 +14,28 @@ struct DefinitionLexicalEntry: Decodable {
 
 struct DefinitionEntry: Decodable {
     let senses: [DefinitionSense]
+    
+    private enum CodingKeys: String, CodingKey {
+        case senses
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let senses = try container.decode([DecodableDefinitionSense].self, forKey: .senses)
+        
+        self.senses = senses.compactMap {
+            $0.definitions
+        }.map {
+            DefinitionSense(definitions: $0)
+        }
+    }
 }
 
-struct DefinitionSense: Decodable {
+struct DecodableDefinitionSense: Decodable {
+    // optional as structure is variant (e.g. subsenses returned by "cat")
+    let definitions: [String]?
+}
+
+struct DefinitionSense {
     let definitions: [String]
 }
