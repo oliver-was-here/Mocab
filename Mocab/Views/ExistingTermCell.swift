a9519fd97dab0261be0274e4812150ffcf19a5b9
Oliver Scott <oliverhbscott@gmail.com>
2018-10-30 00:17:06 -0400

    replace custom ExistinRowCell with Subtitle variant

--- Mocab/Views/ExistingTermCell.swift
+++ Mocab/Views/ExistingTermCell.swift
@@ -6,12 +6,10 @@ class ExistingTermCell: SwipeTableViewCell {
     static let ID = "EXISTING_TERM"
     var modelView: TermModelView?
     
-    @IBOutlet weak var termLabel: UILabel!
-    @IBOutlet weak var definitionLabel: UILabel!
-    
     func configure(modelView: TermModelView, delegate: SwipeTableViewCellDelegate) {
-        termLabel?.text = modelView.term
-        definitionLabel?.text = modelView.definition
+        
+        textLabel?.text = modelView.term
+        detailTextLabel?.text = modelView.definition
         self.modelView = modelView
         self.delegate = delegate
     }
